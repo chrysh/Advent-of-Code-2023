512b2e6e275e150eb97f7a1a8d0c8e77484de807
Christina Quast <contact@christina-quast.de>
2023-12-08 15:52:49 +0100

    Improved sceleton

--- sceleton.rs
+++ sceleton.rs
@@ -1,11 +1,17 @@
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
 
-
-fn sum_tuples(tuples: Vec<u32>) -> u32 {
-    tuples.iter().sum()
+macro_rules! dbg {
+    ($($arg:tt)*) => {
+        #[cfg(debug_assertions)]
+        {
+            println!($($arg)*);
+        }
+    };
 }
+
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
 where
     P: AsRef<Path>,
@@ -14,18 +20,29 @@ where
     Ok(io::BufReader::new(file).lines())
 }
 
+fn read_file_into_lines(filename: &str) -> Result<(Vec<String>, HashMap<String, u32>), Box<dyn std::error::Error>> {
+    let mut lines_vec: Vec<String> = Vec::new();
+    let mut map: HashMap<String, u32> = HashMap::new();
+
+    let lines = read_lines(filename)?;
+
+    for line in lines {
+        let ip = line?;
+        let parts: Vec<&str> = ip.split(' ').collect();
+        lines_vec.push(parts[0].to_string());
+        let num = parts[1].parse::<u32>().map_err(|e| format!("Failed to parse number: {}", e))?;
+        map.insert(parts[0].to_string(), num);
+    }
+
+    Ok((lines_vec, map))
+}
+
 
 fn main() -> io::Result<()> {
-    let mut lines_vec = Vec::new();
+    let lines_vec: Vec<String>;
 
-    if let Ok(lines) = read_lines("input") {
-        for line in lines {
-            if let Ok(ip) = line {
-                println!("{}", ip);
-                lines_vec.push(ip);
-            }
-        }
-    }
+    (lines_vec, _) = read_file_into_lines("input")?;
+
     println!("{}", sum_tuples(lines_vec));
     Ok(())
 }
