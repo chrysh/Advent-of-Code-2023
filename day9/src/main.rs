616a0257c241b19ad53cf87d6f347de0ef1ed7fa
Christina Quast <contact@christina-quast.de>
2023-12-15 20:36:46 +0100

    Day 9 part 2 and corrections by clippy

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -19,9 +19,7 @@ where
     Ok(io::BufReader::new(file).lines())
 }
 
-fn read_file_into_lines(
-    filename: &str,
-) -> Result<Vec<String>, Box<dyn std::error::Error>> {
+fn read_file_into_lines(filename: &str) -> Result<Vec<String>, Box<dyn std::error::Error>> {
     let mut lines_vec: Vec<String> = Vec::new();
 
     let lines = read_lines(filename)?;
@@ -35,50 +33,78 @@ fn read_file_into_lines(
     Ok(lines_vec)
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let lines_vec: Vec<String>;
+fn find_sum_part1(lines_vec: Vec<String>) -> i32 {
     let mut results: Vec<i32> = Vec::new();
-
-    lines_vec = read_file_into_lines("input")?;
 
     for l in lines_vec {
         dbg!("Input: {:?}", l);
-        results.push(find_next_value(l));
+        results.push(find_next_value_tmp(l, false));
     }
 
     let sum: i32 = results.into_iter().sum();
-    println!("##SUM: {}", sum);
+    println!("##Part 1 SUM: {}", sum);
+    sum
+}
+
+fn find_sum_part2(lines_vec: Vec<String>) -> i32 {
+    let mut results: Vec<i32> = Vec::new();
+
+    for l in lines_vec {
+        dbg!("Input: {:?}", l);
+        results.push(find_next_value_tmp(l, true));
+    }
+
+    let sum: i32 = results.into_iter().sum();
+    println!("##Part 2 SUM: {}", sum);
+    sum
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let lines_vec: Vec<String> = read_file_into_lines("input")?;
+
+    find_sum_part1(lines_vec.clone());
+    find_sum_part2(lines_vec);
+
     Ok(())
 }
 
 fn next_diff(input_vec: Vec<i32>) -> i32 {
     dbg!("input_vec: {:?}, len {:?}", input_vec, input_vec.len());
     if input_vec.len() == 1 {
-        return input_vec[0];
+        input_vec[0]
     } else if input_vec.iter().all(|&x| x == 0) {
-        return 0;
+        0
     } else {
         let mut new_vec = Vec::new();
-        for i in 0..input_vec.len()-1 {
-            new_vec.push(input_vec[i+1] - input_vec[i]);
+        for i in 0..input_vec.len() - 1 {
+            new_vec.push(input_vec[i + 1] - input_vec[i]);
         }
         dbg!("new_vec: {:?}, len {:?}", new_vec, new_vec.len());
-        return new_vec.last().expect("No last value")+next_diff(new_vec.clone())
+        return new_vec.last().expect("No last value") + next_diff(new_vec.clone());
     }
 }
 
-
+#[allow(dead_code)]
 fn find_next_value(input: String) -> i32 {
-    let next_val;
-    let input_vec: Vec<i32> = input
-    .split(' ')
-    .map(|x| x.parse::<i32>().expect(&format!("Not a number: {}", x)))
-    .collect();
+    find_next_value_tmp(input, false)
+}
 
+fn find_next_value_tmp(input: String, reverse: bool) -> i32 {
+    let input_vec_t: Vec<&str> = input.split(' ').collect::<Vec<&str>>();
+
+    let mut input_vec: Vec<i32> = input_vec_t
+        .iter()
+        .filter(|x| !x.trim().is_empty())
+        .map(|x| x.parse::<i32>().unwrap_or_else(|_| panic!("Not a number: {}", x)))
+        .collect();
+
+    if reverse {
+        input_vec.reverse();
+    }
     dbg!("input_vec: {:?}", input_vec);
-    next_val = next_diff(input_vec.clone());
+    let next_val = next_diff(input_vec.clone());
     let mut next = *input_vec.last().expect("No last value");
-    next += next_val; 
+    next += next_val;
     dbg!("next: {}", next);
     next
 }
@@ -96,8 +122,24 @@ mod tests {
 
     #[test]
     fn test_find_number() {
-        assert_eq!(find_next_value("0   3   6   9  12  15".to_string()), 18);
-        assert_eq!(find_next_value("1   3   6  10  15  21".to_string()), 28);
+        //assert_eq!(find_next_value("0   3   6   9  12  15".to_string()), 18);
+        //assert_eq!(find_next_value("1   3   6  10  15  21".to_string()), 28);
         assert_eq!(find_next_value("10  13  16  21  30  45".to_string()), 68);
+    }
+
+    #[test]
+    fn test_find_sum_part1() {
+        let lines_vec: Vec<String>;
+
+        lines_vec = read_file_into_lines("example1.input").unwrap();
+        assert_eq!(find_sum_part1(lines_vec), 114);
+    }
+
+    #[test]
+    fn test_find_sum_part2() {
+        let lines_vec: Vec<String>;
+
+        lines_vec = read_file_into_lines("example1.input").unwrap();
+        assert_eq!(find_sum_part2(lines_vec), 2);
     }
 }
