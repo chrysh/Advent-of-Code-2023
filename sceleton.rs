9f0203411d1e54d9d56f835dec9cc5e258aed35e
Christina Quast <contact@christina-quast.de>
2023-12-22 13:38:34 +0100

    Update sceleton

--- sceleton.rs
+++ sceleton.rs
@@ -1,7 +1,7 @@
-use std::collections::HashMap;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
+use scan_fmt::scan_fmt;
 
 macro_rules! dbg {
     ($($arg:tt)*) => {
@@ -20,40 +20,75 @@ where
     Ok(io::BufReader::new(file).lines())
 }
 
-fn read_file_into_lines(filename: &str) -> Result<(Vec<String>, HashMap<String, u32>), Box<dyn std::error::Error>> {
-    let mut lines_vec: Vec<String> = Vec::new();
-    let mut map: HashMap<String, u32> = HashMap::new();
-
-    let lines = read_lines(filename)?;
-
-    for line in lines {
-        let ip = line?;
-        let parts: Vec<&str> = ip.split(' ').collect();
-        lines_vec.push(parts[0].to_string());
-        let num = parts[1].parse::<u32>().map_err(|e| format!("Failed to parse number: {}", e))?;
-        map.insert(parts[0].to_string(), num);
-    }
-
-    Ok((lines_vec, map))
+#[derive(Debug)]
+enum Direction {
+    U,
+    D,
+    L,
+    R,
 }
 
+impl Direction {
+    fn from_char(c: char) -> Option<Self> {
+        match c {
+            'U' => Some(Self::U),
+            'D' => Some(Self::D),
+            'L' => Some(Self::L),
+            'R' => Some(Self::R),
+            _ => None,
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Step {
+    dir: Direction,
+    steps: u32,
+    rgb: String,
+}
+
+fn read_file_into_vec(filename: &str) -> Result<Vec<Step>, Box<dyn std::error::Error>> {
+    let lines = read_lines(filename)?;
+    let steps: Vec<Step> = lines.into_iter().filter_map(|l|
+        { match l {
+            Ok(l) => match parse_input(&l) {
+                Ok(direction) => Some(direction),
+                Err(e) => {
+                    eprintln!("Failed to parse line: {}", e);
+                    None
+                }
+            },
+            Err(e) => {
+                eprintln!("Failed to read line: {}", e);
+                None
+            },
+        }
+        }).collect();
+
+    dbg!("{:?}", steps);
+    Ok(steps)
+}
+
+fn parse_input(input: &str) -> Result<Step, Box<dyn std::error::Error>> {
+    let (character, steps, rgb) = scan_fmt!(input, "{[a-zA-Z]} {d} (#{[a-zA-Z0-9]})", char, u32, String)?;
+    let dir = Direction::from_char(character).ok_or("Failed to parse direction")?;
+    Ok(Step { dir, steps, rgb })
+}
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let lines_vec: Vec<String>;
+    let input = read_file_into_vec("input")?;
 
-    (lines_vec, _) = read_file_into_lines("input")?;
-
-    println!("{}", sum_tuples(lines_vec));
+    // println!("PART1: {:?}", find_sum(patterns));
+    // println!("PART2 Sum: {}", run_part2_threads());
     Ok(())
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
     #[test]
     fn test_find_number() {
-        let input = 12;
+        let input = read_file_into_lines("example1.input").expect("Failed to read file");
         assert_eq!(input, 12);
     }
 
