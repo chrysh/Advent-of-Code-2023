3bdf9dd60e901f742a80c7e941c6fc8c90b6f28d
Christina Quast <contact@christina-quast.de>
2024-01-23 17:11:45 +0100

    solution day1 part2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -10,21 +10,56 @@ fn parse_number(left: Option<char>, right: Option<char>) -> Option<u32> {
     }
 }
 
+fn map_word2num(s: &str) -> String {
+    let number_words = [("zero", "0"), ("one", "1"), ("two", "2"), ("three", "3"), ("four", "4"), ("five", "5"), ("six", "6"), ("seven", "7"), ("eight", "8"), ("nine", "9")];
+    let mut result = s.to_string();
+
+    loop {
+        let mut res_pos = None;
+        let mut res_word = "";
+        let mut res_num = "";
+
+        for (word, num) in number_words.iter() {
+            if let Some(pos) = result.find(word) {
+                if res_pos.map_or(true, |p| pos < p) {
+                    res_pos = Some(pos);
+                    res_word = word;
+                    res_num = num;
+                } 
+            }
+        }
+        if let Some(_pos) = res_pos {
+            result = result.replacen(&res_word[0..res_word.len()-1], res_num, 1);
+        } else {
+            break;
+        }
+    }
+
+    result
+}
+
 fn find_number(s: &str) -> Option<u32> {
-    let mut left: Option<char> = None;
-    let mut right: Option<char> = None;
+    let mut first: Option<char> = None;
+    let mut last: Option<char> = None;
     for c in s.chars() {
-        if c.is_numeric() && left.is_none() {
-            left = Some(c);
-        } else if c.is_numeric() {
-            right = Some(c);
+        if c.is_numeric() {
+            if first.is_none() {
+                first = Some(c);
+            }
+            last = Some(c);
         }
     };
-    parse_number(left, right)
+    let ret = parse_number(first, last);
+    ret
 }
 
 fn extract_numbers(list: Vec<String>) -> impl Iterator<Item = u32> {
     list.into_iter().filter_map(|s| find_number(&s))
+}
+
+fn extract_numbers_part2(list: Vec<String>) -> impl Iterator<Item = u32> {
+
+    list.into_iter().map(|s| map_word2num(&s)).filter_map(|s| find_number(&s))
 }
 
 fn sum_tuples<I>(tuples: I) -> u32 where I: Iterator<Item = u32> {
@@ -39,21 +74,60 @@ where
     Ok(io::BufReader::new(file).lines())
 }
 
-
-fn main() -> io::Result<()> {
-    let lines_vec: Vec<_> = read_lines("input")
+fn read_lines_vec(filename: &str) -> Vec<String> {
+    let lines_vec: Vec<_> = read_lines(filename)
         .unwrap()
         .filter_map(|line| line.ok())
-        .inspect(|ip| { println!("{ip:?}"); })
+        //.inspect(|ip| { println!("{ip:?}"); })
         .collect();
+    lines_vec
+}
 
-    println!("{}", sum_tuples(extract_numbers(lines_vec)));
+fn main() -> io::Result<()> {
+    let lines_vec = read_lines_vec("input");
+
+    println!("PART1: {}", sum_tuples(extract_numbers(lines_vec.clone())));
+    println!("PART2: {}", sum_tuples(extract_numbers_part2(lines_vec)));
     Ok(())
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_word2num_part2() {
+        assert_eq!(sum_tuples(extract_numbers_part2(vec!["one5qdtnrpcmrnnsbeighttwotwoninegtpv".to_string()])), 19);
+        assert_eq!(sum_tuples(extract_numbers_part2(vec!["eightwothree".to_string()])), 83);
+        assert_eq!(sum_tuples(extract_numbers_part2(vec!["oneight".to_string()])), 18);
+        assert_eq!(sum_tuples(extract_numbers_part2(vec!["twone".to_string()])), 21);
+        assert_eq!(sum_tuples(extract_numbers_part2(vec!["twone".to_string()])), 21);
+        assert_eq!(sum_tuples(extract_numbers_part2(vec!["nineight".to_string()])), 98);
+        assert_eq!(sum_tuples(extract_numbers_part2(vec!["fiveight".to_string()])), 58);
+        assert_eq!(sum_tuples(extract_numbers_part2(vec!["sevenine".to_string()])), 79);
+        assert_eq!(sum_tuples(extract_numbers_part2(vec!["twone".to_string()])), 21);
+        assert_eq!(sum_tuples(extract_numbers_part2(vec!["9abc".to_string()])), 99);
+        assert_eq!(sum_tuples(extract_numbers_part2(vec!["9".to_string()])), 99);
+    }
+
+    #[test]
+    fn test_example_part22() {
+        let lines_vec = read_lines_vec("input");
+        assert_eq!(sum_tuples(extract_numbers_part2(lines_vec)), 53592);
+    }
+
+    #[test]
+    fn test_example_part2() {
+        let lines_vec = read_lines_vec("example2.input");
+        assert_eq!(sum_tuples(extract_numbers_part2(lines_vec)), 281);
+    }
+
+    #[test]
+    fn test_example_part1() {
+        let lines_vec = read_lines_vec("example1.input");
+        assert_eq!(sum_tuples(extract_numbers(lines_vec)), 142);
+    }
+
     #[test]
     fn test_find_number() {
         assert_eq!(find_number("1abc2"), Some(12));
@@ -72,5 +146,4 @@ mod tests {
             .collect();
         assert_eq!(sum_tuples(extract_numbers(input)), 142);
     }
-
 }
